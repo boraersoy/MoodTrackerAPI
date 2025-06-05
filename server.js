185dcf54ff4baea6a78516f3a0e39b884c2997af
usamaabusnena <osamasnina@gmail.com>
2025-06-05 20:25:29 +0300

    add hello message

--- server.js
+++ server.js
@@ -23,6 +23,11 @@ app.use('/api', routes);
 
 // Start server
 const PORT = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+  res.send('Welcome to My Day');
+});
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
